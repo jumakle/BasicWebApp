40163ce72b7570e011f34587e8590f7975e8333b
ge96xob <julian_markus.klein@tum.de>
2022-07-13 13:23:04 +0200

    Test Commit

--- src/main/java/de/tum/in/ase/eist/QueryProcessor.java
+++ src/main/java/de/tum/in/ase/eist/QueryProcessor.java
@@ -13,9 +13,29 @@ public class QueryProcessor {
                     "writer in the English language and the world's pre-eminent dramatist.";
         } else if (query.contains("name")) {
            return "julian";
+        } else if (query.contains("plus")){
+            //query = "what is 9 plus 3"
+            String[] wordList = query.split(" ");
+            int sum = 0;
+            for (String word : wordList){
+                try{
+                    int intOfWord = Integer.parseInt(word);
+                    sum += intOfWord;
+                }catch (Exception e){
+
+                }
+
+            }
+           return String.valueOf(sum);
         } else { // TODO extend the programm here
             return "";
+
         }
         //hello world again
+        //Integer.parseInt("myString 2")
+        //String nine = query.substring(8, 1);
+        //String three = query.substring(13, 1);
+        //int result = Integer.parseInt(nine);
+        //return String.valueOf(result);
     }
 }
